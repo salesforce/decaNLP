ee3ba6c1b53857e88f9fc323844e26f96ca5607a
Bryan Marcus McCann <bryan.mccann.is@gmail.com>
2018-09-18 00:39:41 +0000

    rm Variable from pg

--- models/multitask_question_answering_network.py
+++ models/multitask_question_answering_network.py
@@ -172,44 +172,43 @@ class MultitaskQuestionAnsweringNetwork(nn.Module):
 
 
     def greedy(self, self_attended_context, context, question, context_indices, question_indices, oov_to_limited_idx, rnn_state=None):
-        with torch.no_grad():
-            B, TC, C = context.size()
-            T = self.args.max_output_length
-            outs = context.new_full((B, T), self.field.decoder_stoi['<pad>'], dtype=torch.long)
-            hiddens = [self_attended_context[0].new_zeros((B, T, C))
-                       for l in range(len(self.self_attentive_decoder.layers) + 1)]
-            hiddens[0] = hiddens[0] + positional_encodings_like(hiddens[0])
-            eos_yet = context.new_zeros((B, )).byte()
+        B, TC, C = context.size()
+        T = self.args.max_output_length
+        outs = context.new_full((B, T), self.field.decoder_stoi['<pad>'], dtype=torch.long)
+        hiddens = [self_attended_context[0].new_zeros((B, T, C))
+                   for l in range(len(self.self_attentive_decoder.layers) + 1)]
+        hiddens[0] = hiddens[0] + positional_encodings_like(hiddens[0])
+        eos_yet = context.new_zeros((B, )).byte()
     
-            rnn_output, context_alignment, question_alignment = None, None, None
-            for t in range(T):
-                if t == 0:
-                    embedding = self.decoder_embeddings(
-                        self_attended_context[-1].new_full((B, 1), self.field.vocab.stoi['<init>'], dtype=torch.long), [1]*B)
-                else:
-                    embedding = self.decoder_embeddings(outs[:, t - 1].unsqueeze(1), [1]*B)
-                hiddens[0][:, t] = hiddens[0][:, t] + (math.sqrt(self.self_attentive_decoder.d_model) * embedding).squeeze(1)
-                for l in range(len(self.self_attentive_decoder.layers)):
-                    hiddens[l + 1][:, t] = self.self_attentive_decoder.layers[l].feedforward(
-                        self.self_attentive_decoder.layers[l].attention(
-                        self.self_attentive_decoder.layers[l].selfattn(hiddens[l][:, t], hiddens[l][:, :t + 1], hiddens[l][:, :t + 1])
-                      , self_attended_context[l], self_attended_context[l]))
-                decoder_outputs = self.dual_ptr_rnn_decoder(hiddens[-1][:, t].unsqueeze(1),
-                    context, question, 
-                    context_alignment=context_alignment, question_alignment=question_alignment,
-                    hidden=rnn_state, output=rnn_output)
-                rnn_output, context_attention, question_attention, context_alignment, question_alignment, vocab_pointer_switch, context_question_switch, rnn_state = decoder_outputs
-                probs = self.probs(self.out, rnn_output, vocab_pointer_switch, context_question_switch, 
-                    context_attention, question_attention, 
-                    context_indices, question_indices, 
-                    oov_to_limited_idx)
-                pred_probs, preds = probs.max(-1)
-                preds = preds.squeeze(1)
-                eos_yet = eos_yet | (preds == self.field.decoder_stoi['<eos>'])
-                outs[:, t] = preds.cpu().apply_(self.map_to_full)
-                if eos_yet.all():
-                    break
-            return outs
+        rnn_output, context_alignment, question_alignment = None, None, None
+        for t in range(T):
+            if t == 0:
+                embedding = self.decoder_embeddings(
+                    self_attended_context[-1].new_full((B, 1), self.field.vocab.stoi['<init>'], dtype=torch.long), [1]*B)
+            else:
+                embedding = self.decoder_embeddings(outs[:, t - 1].unsqueeze(1), [1]*B)
+            hiddens[0][:, t] = hiddens[0][:, t] + (math.sqrt(self.self_attentive_decoder.d_model) * embedding).squeeze(1)
+            for l in range(len(self.self_attentive_decoder.layers)):
+                hiddens[l + 1][:, t] = self.self_attentive_decoder.layers[l].feedforward(
+                    self.self_attentive_decoder.layers[l].attention(
+                    self.self_attentive_decoder.layers[l].selfattn(hiddens[l][:, t], hiddens[l][:, :t + 1], hiddens[l][:, :t + 1])
+                  , self_attended_context[l], self_attended_context[l]))
+            decoder_outputs = self.dual_ptr_rnn_decoder(hiddens[-1][:, t].unsqueeze(1),
+                context, question, 
+                context_alignment=context_alignment, question_alignment=question_alignment,
+                hidden=rnn_state, output=rnn_output)
+            rnn_output, context_attention, question_attention, context_alignment, question_alignment, vocab_pointer_switch, context_question_switch, rnn_state = decoder_outputs
+            probs = self.probs(self.out, rnn_output, vocab_pointer_switch, context_question_switch, 
+                context_attention, question_attention, 
+                context_indices, question_indices, 
+                oov_to_limited_idx)
+            pred_probs, preds = probs.max(-1)
+            preds = preds.squeeze(1)
+            eos_yet = eos_yet | (preds == self.field.decoder_stoi['<eos>'])
+            outs[:, t] = preds.cpu().apply_(self.map_to_full)
+            if eos_yet.all():
+                break
+        return outs
 
 
 
