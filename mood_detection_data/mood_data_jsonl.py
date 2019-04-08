718fecd8ba3dc2ff08d5228ce13d8c7ee71fb87d
Lingsong Gao <lg5@illinois.com>
2019-04-07 23:41:37 -0400

    jsonl files

---
+++ mood_detection_data/mood_data_jsonl.py
@@ -0,0 +1,46 @@
+import jsonlines
+
+question = "What’s the tweet’s emotion, \
+    angry or fearful or joyful or sad, and what’s the intensity level, slightly or fairly or extremely?"
+
+mood = {'anger':'angry', 'fear':'fearful', 'joy':'joyful', 'sadness':'sad'}
+
+filepath = ["train/anger_train.txt", "train/fear_train.txt", "train/joy_train.txt", "train/sadness_train.txt"]
+
+displaynum = 10
+
+output_path = 'train.jsonl' #'.data/mood_detection_dataset/val.jsonl'
+
+def label(its, noun):
+    if its >= 0 and its < 0.33:
+        return "slight " + mood[noun]
+    elif its >= 0.33 and its < 0.67:
+        return "fairly " + mood[noun]
+    else:
+        return "extremely " + mood[noun]
+
+def to_jsonl():
+    for file in filepath:
+        num = 0
+        with open(file) as f:
+            with jsonlines.open(output_path, mode='a') as writer:
+                for line in f.readlines():
+                    l = line.split()
+                    context = ' '.join(l[1:-2])
+                    answer = label(float(l[-1]), l[-2])
+                    writer.write({"context": context, "question": question, "answer": answer})
+                    num += 1
+        print(f"this file contains {num} lines")
+
+def display_jsonl():
+    with jsonlines.open('output.jsonl', mode='r') as reader:
+        i = 0
+        for obj in reader:
+            if i <= displaynum:
+                print(obj)
+                i += 1
+            else: 
+                break
+
+if __name__ == '__main__':
+    to_jsonl()
